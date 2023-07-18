95bbb5249deda94dbc171fa8e0a0c850e69e177f
nklgerasimov <gniko@yandex.ru>
2023-07-18 13:27:59 +0300

    init

--- gulpfile.js
+++ gulpfile.js
@@ -54,6 +54,16 @@ gulp.task('images', function() {
         .pipe(gulp.dest('./dist/img/'))
 })
 
+gulp.task('fonts', function() {
+    return gulp.src('./src/fonts/**/*')
+        .pipe(gulp.dest('./dist/fonts/'))
+})
+
+gulp.task('files', function() {
+    return gulp.src('./src/files/**/*')
+        .pipe(gulp.dest('./dist/files/'))
+})
+
 gulp.task('server', function () {
     return gulp.src('./dist/').pipe(server({
         livereload: true,
@@ -72,10 +82,12 @@ gulp.task('watch', function() {
     gulp.watch('./src/scss/**/*.scss', gulp.parallel('sass'));
     gulp.watch('./src/**/*.html', gulp.parallel('html'));
     gulp.watch('./src/img/**/*', gulp.parallel('images'));
+    gulp.watch('./src/fonts/**/*', gulp.parallel('fonts'));
+    gulp.watch('./src/files/**/*', gulp.parallel('files'));
 })
 
 gulp.task('default', gulp.series(
     'clean',
-    gulp.parallel('html', 'sass', 'images'),
+    gulp.parallel('html', 'sass', 'images', 'fonts', 'files'),
     gulp.parallel('server', 'watch'),
 ))
