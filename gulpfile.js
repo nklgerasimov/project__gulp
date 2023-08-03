ce95bfd3aa30f3e5d4a0fe5168153db3163a3e90
nklgerasimov <gniko@yandex.ru>
2023-08-03 11:58:28 +0300

    final

--- gulpfile.js
+++ gulpfile.js
@@ -1,93 +1,19 @@
 const gulp = require('gulp');
-const fileInclude = require('gulp-file-include');
-const sass =require('gulp-sass')(require('sass'));
-const server = require('gulp-server-livereload');
-const clean = require('gulp-clean');
-const fs = require('fs');
-const sourceMaps = require('gulp-sourcemaps');
-// const groupMedia = require('gulp-group-css-media-queries');
-const plumber = require('gulp-plumber');
-const notify = require('gulp-notify');
+require('./gulp/dev.js');
+require('./gulp/docs.js');
 
-const fileIncludeSetting = {
-    prefix: '@@',
-    basepath: '@file'
-}
+gulp.task(
+    'default',
+    gulp.series(
+    'clean:dev',
+    gulp.parallel('html:dev', 'sass:dev', 'images:dev', 'fonts:dev', 'files:dev', 'js:dev'),
+    gulp.parallel('server:dev', 'watch:dev'),
+))
 
-const plumberHtmlConfig = {
-    errorHandler: notify.onError({
-        title: 'HTML',
-        message: 'Error <%= error.message %>',
-        sound: false,
-    })
-}
-
-gulp.task('html', function() {
-    return gulp
-        .src('./src/*.html')
-        .pipe(plumber(plumberHtmlConfig))
-        .pipe(fileInclude(fileIncludeSetting))
-        .pipe(gulp.dest('./dist/'))
-});
-
-const plumberSassConfig = {
-    errorHandler: notify.onError({
-        title: 'Styles',
-        message: 'Error <%= error.message %>',
-        sound: false
-    })
-}
-
-gulp.task('sass', function(){
-    return gulp
-        .src('./src/scss/*.scss')
-        .pipe(plumber(plumberSassConfig))
-        .pipe(sourceMaps.init())
-        .pipe(sass())
-        // .pipe(groupMedia())
-        .pipe(sourceMaps.write())
-        .pipe(gulp.dest('./dist/css/'))
-})
-
-gulp.task('images', function() {
-    return gulp.src('./src/img/**/*')
-        .pipe(gulp.dest('./dist/img/'))
-})
-
-gulp.task('fonts', function() {
-    return gulp.src('./src/fonts/**/*')
-        .pipe(gulp.dest('./dist/fonts/'))
-})
-
-gulp.task('files', function() {
-    return gulp.src('./src/files/**/*')
-        .pipe(gulp.dest('./dist/files/'))
-})
-
-gulp.task('server', function () {
-    return gulp.src('./dist/').pipe(server({
-        livereload: true,
-        open: true
-    }))
-})
-
-gulp.task('clean', function(done) {
-    if (fs.existsSync('./dist/')) {
-        return gulp.src('./dist/', {read: false}).pipe(clean({force: true}));
-    }
-    done();
-})
-
-gulp.task('watch', function() {
-    gulp.watch('./src/scss/**/*.scss', gulp.parallel('sass'));
-    gulp.watch('./src/**/*.html', gulp.parallel('html'));
-    gulp.watch('./src/img/**/*', gulp.parallel('images'));
-    gulp.watch('./src/fonts/**/*', gulp.parallel('fonts'));
-    gulp.watch('./src/files/**/*', gulp.parallel('files'));
-})
-
-gulp.task('default', gulp.series(
-    'clean',
-    gulp.parallel('html', 'sass', 'images', 'fonts', 'files'),
-    gulp.parallel('server', 'watch'),
+gulp.task(
+    'docs',
+    gulp.series(
+    'clean:docs',
+    gulp.parallel('html:docs', 'sass:docs', 'images:docs', 'fonts:docs', 'files:docs', 'js:docs'),
+    gulp.parallel('server:docs'),
 ))
